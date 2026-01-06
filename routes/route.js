d72f59e7a74e042d25e722aab2fa8404ed0b6221
Kajal Gajare <kajal@8848digital.com>
2026-01-06 12:22:32 +0530

    fix : advEvent complete flow

--- routes/route.js
+++ routes/route.js
@@ -58,7 +58,5 @@ router.get('/getBasePath', getBasePath);
 const { getAdvEvents } = require('../controllers/getAdvEvents');
 router.get('/getAdvEvents', transactionalControllerWrapper(getAdvEvents));
 
-const { getYPOS } = require('../controllers/yPosController');
-router.get('/getYPOS', transactionalControllerWrapper(getYPOS));
 
 module.exports = router;
